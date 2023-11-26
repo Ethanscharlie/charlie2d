60a118b46308ac86e08fc725f845ac01f7be1bc6
Ethanscharlie <ethanscharlie@protonmail.com>
2023-11-26 10:08:41 -0700

    Finished Docs

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,15 +1,20 @@
 var searchData=
 [
-  ['changescale_0',['changeScale',['../structBox.html#a8cbd3f8ec11e4811eb29f9a79295780a',1,'Box']]],
-  ['checkaxis_1',['checkAxis',['../classInputManager.html#af52401c3d20a16b6011f04f9681afaa8',1,'InputManager']]],
-  ['checkboxcollision_2',['checkBoxCollision',['../classCollider.html#aefba4ca2e639502bb578461ffe6e5117',1,'Collider']]],
-  ['checkcirclecollision_3',['checkCircleCollision',['../classCircleCollider.html#abc7a4b982b2a817dbbec64922959e06b',1,'CircleCollider']]],
-  ['checkcollision_4',['checkcollision',['../classCircleCollider.html#a67faf2aad5906fd1c5a004e94d8f1e50',1,'CircleCollider::checkCollision()'],['../classCollider.html#a4e15234bb7d94468dc3060e98944c13f',1,'Collider::checkCollision()'],['../structBox.html#a72eaeb8321c2ecbada97f2470f74b633',1,'Box::checkCollision()']]],
-  ['checkcomponent_5',['checkComponent',['../classEntity.html#aaa08bd4404c74e38d426fb395411c3a6',1,'Entity']]],
-  ['checkhorizontal_6',['checkHorizontal',['../classInputManager.html#abba7a775276179958f0aea1ebeff9614',1,'InputManager']]],
-  ['checkinput_7',['checkInput',['../classInputManager.html#a59438046010e85ae15abf22de7c11644',1,'InputManager']]],
-  ['checkoutsidebounds_8',['checkOutsideBounds',['../classLDTK.html#a4a7f1156ceac70f9acb72e4a37c2953b',1,'LDTK']]],
-  ['checkvertical_9',['checkVertical',['../classInputManager.html#a07f199764155999b43a7bb4e23131d77',1,'InputManager']]],
-  ['clear_10',['clear',['../classResourceManager.html#a50cf42daa0ff355fef64431740e498ab',1,'ResourceManager']]],
-  ['createentity_11',['createEntity',['../classScene.html#aea5510be8aa57aae76558f6228c665c1',1,'Scene']]]
+  ['changefont_0',['changeFont',['../classText.html#a902c7fd99475f2628a9c565bf1da959b',1,'Text']]],
+  ['changelocalposition_1',['changeLocalPosition',['../structentityBox.html#a842a4940c2f05f33a50aa5bd56a775b7',1,'entityBox']]],
+  ['changelocalsize_2',['changeLocalSize',['../structentityBox.html#a1ad8dac728244be68bcc67b726fe389d',1,'entityBox']]],
+  ['changeposition_3',['changePosition',['../structentityBox.html#ab7247902b1ee32ec32cf72abeaa90a99',1,'entityBox']]],
+  ['changescale_4',['changescale',['../structentityBox.html#aab92e11f77f67bd2411b399d7ce83a8b',1,'entityBox::changeScale()'],['../structBox.html#a8cbd3f8ec11e4811eb29f9a79295780a',1,'Box::changeScale()']]],
+  ['changesize_5',['changeSize',['../structentityBox.html#a61c0ac45c85302a7422bcc439de1ea44',1,'entityBox']]],
+  ['checkaxis_6',['checkAxis',['../classInputManager.html#af52401c3d20a16b6011f04f9681afaa8',1,'InputManager']]],
+  ['checkboxcollision_7',['checkBoxCollision',['../classCollider.html#aefba4ca2e639502bb578461ffe6e5117',1,'Collider']]],
+  ['checkcirclecollision_8',['checkCircleCollision',['../classCircleCollider.html#abc7a4b982b2a817dbbec64922959e06b',1,'CircleCollider']]],
+  ['checkcollision_9',['checkcollision',['../classCircleCollider.html#a67faf2aad5906fd1c5a004e94d8f1e50',1,'CircleCollider::checkCollision()'],['../structBox.html#a72eaeb8321c2ecbada97f2470f74b633',1,'Box::checkCollision()'],['../classCollider.html#a4e15234bb7d94468dc3060e98944c13f',1,'Collider::checkCollision()']]],
+  ['checkcomponent_10',['checkComponent',['../classEntity.html#aaa08bd4404c74e38d426fb395411c3a6',1,'Entity']]],
+  ['checkhorizontal_11',['checkHorizontal',['../classInputManager.html#abba7a775276179958f0aea1ebeff9614',1,'InputManager']]],
+  ['checkinput_12',['checkInput',['../classInputManager.html#a59438046010e85ae15abf22de7c11644',1,'InputManager']]],
+  ['checkoutsidebounds_13',['checkOutsideBounds',['../classLDTK.html#a4a7f1156ceac70f9acb72e4a37c2953b',1,'LDTK']]],
+  ['checkvertical_14',['checkVertical',['../classInputManager.html#a07f199764155999b43a7bb4e23131d77',1,'InputManager']]],
+  ['clear_15',['clear',['../classResourceManager.html#a50cf42daa0ff355fef64431740e498ab',1,'ResourceManager']]],
+  ['createentity_16',['createEntity',['../classScene.html#aea5510be8aa57aae76558f6228c665c1',1,'Scene']]]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['randfloat_0',['randFloat',['../Math_8h.html#a2f5480bd60924b64a3793e77d985f692',1,'Math.h']]],
-  ['require_1',['require',['../classEntity.html#abe66cdc587c335147849833216ca5c77',1,'Entity']]],
-  ['resetframecounter_2',['resetFrameCounter',['../classAnimation.html#ad53139f47d0156675d9e86cad7a9ec89',1,'Animation']]],
-  ['rotate_3',['rotate',['../structAngle.html#abf6535aa28888477e87c8fe749b3a402',1,'Angle']]]
+  ['rendertextinrect_1',['renderTextInRect',['../classText.html#a41ddc0c12816b74ecf2b06f12ef08ae3',1,'Text']]],
+  ['require_2',['require',['../classEntity.html#abe66cdc587c335147849833216ca5c77',1,'Entity']]],
+  ['resetframecounter_3',['resetFrameCounter',['../classAnimation.html#ad53139f47d0156675d9e86cad7a9ec89',1,'Animation']]],
+  ['rotate_4',['rotate',['../structAngle.html#abf6535aa28888477e87c8fe749b3a402',1,'Angle']]]
 ];
